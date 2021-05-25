e0c966a4647062518369385b455f92a985cb63d3
Steven Yuen <steven_yuen05@hotmail.com>
2021-05-25 01:28:23 -0400

    updated fix

--- 2ddev_setup.sh
+++ 2ddev_setup.sh
@@ -4,7 +4,7 @@ apik=""
 sudo DD_AGENT_MAJOR_VERSION=7 DD_API_KEY=$apik bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
 sudo usermod -a -G vagrant dd-agent
 sudo datadog-agent status
-sudo chmod 744 /etc/datadog-agent/auth_token
+sudo chmod -R 744 /etc/datadog-agent/
 sudo service datadog-agent restart
 ddev config set dd_api_key $apik
 # /home/vagrant/.pyenv/shims/ddev config set dd_api_key $apik
